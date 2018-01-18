ee4984e3ed1c3c8b1af1d7a6d2c09f6d6c9c8ba0
Sarachan, Brion D <sarachan@ge.com>
2018-01-18 09:18:11 -0500

    Changed Docker image prefix to thriveitcr to match Docker Hub.

--- algrebuild.sh
+++ algrebuild.sh
@@ -3,11 +3,11 @@
 
 echo ""
 echo "Removing algorithm image."
-docker rmi thrive/multicompartment-cell-quant
+docker rmi thriveitcr/multicompartment-cell-quant
 
 echo ""
 echo "Building algorithm image."
-docker build -t thrive/multi-compartment-cell-quantification --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg no_proxy=$no_proxy .
+docker build -t thriveitcr/multi-compartment-cell-quantification --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg no_proxy=$no_proxy .
 
 echo ""
 docker images
